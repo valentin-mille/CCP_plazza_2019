bf3853fc37b75e7cfde79dd2c5d4bdea215c62aa
theprojectguy <valentin.mille@epitech.eu>
2020-05-07 13:06:35 +0000

    [ADD] exit properly on reception

--- src/Reception/Reception.cpp
+++ src/Reception/Reception.cpp
@@ -92,8 +92,6 @@ bool Reception::getShellActivity()
 
 void Reception::displayKitchensStatus()
 {
-    // print the number the current occupancy of the cooks, as well as theirs
-    // stocks of ingredients
     checkKitchensProcessus();
     for (size_t i = 0; i < streamCom_.size(); ++i) {
         streamCom_[i].writeToKitchenBuffer("status");
@@ -133,11 +131,9 @@ int Reception::createNewKitchenProcess(const std::string &currentPizza,
             multiplier_, nbOfCooks_, deliveryTime_, currentStream);
         newKitchen.get()->update();
         newKitchen.reset();
-        std::cout << "====> END OF KITCHEN" << std::endl;
         // Exit to close the child process and destroy the kitchen
         exit(EXIT_SUCCESS);
     } else if (pid > 0) {
-        debug.printOutput("======> Create New kitchen !");
         streamCom_.emplace_back(currentStream);
         kitchensPid_.push_back(pid);
         while (infos == "OK" && nbPizzas > 0) {
@@ -147,8 +143,6 @@ int Reception::createNewKitchenProcess(const std::string &currentPizza,
                 --nbPizzas;
             }
         }
-        debug.printOutput("==> End of kitchens created: " +
-                          std::to_string(nbPizzas));
     } else {
         std::cerr << "==> New Kitchen process failure: " << strerror(errno)
                   << std::endl;
@@ -161,12 +155,8 @@ void Reception::checkKitchensProcessus()
 {
     size_t i = 0;
 
-    debug.printOutput("===> Nb kitchen processes " +
-                      std::to_string(kitchensPid_.size()));
     while (i < kitchensPid_.size()) {
-        debug.printOutput("==> Kitchen processes " + std::to_string(i));
         if (Process::isProcessRunning(kitchensPid_[i]) == false) {
-            debug.printOutput("Erase pid: " + std::to_string(kitchensPid_[i]));
             kitchensPid_.erase(kitchensPid_.begin() + i);
             streamCom_.erase(streamCom_.begin() + i);
             continue;
@@ -178,26 +168,17 @@ void Reception::checkKitchensProcessus()
 int Reception::sendPizzasToKitchens()
 {
     std::string currentPizza = pizzas_.front();
-    debug.printOutput(currentPizza);
     size_t nbPizzas = getNumberOfPizza(currentPizza);
     std::string pizzaTypeSize(getPizzaTypeSize(currentPizza));
     std::string infos("OK");
     std::string serializedOrder;
 
-    debug.printOutput("Number of Cook: " + std::to_string(nbOfCooks_));
     checkKitchensProcessus();
-    debug.printOutput("=======> There is " + std::to_string(streamCom_.size()) +
-                      " Kitchens");
     serializedOrder = InterProcessCom::pack(currentPizza);
-    debug.printOutput("Serialized order " + serializedOrder);
-    debug.printOutput("Nb Pizzas #1: " + std::to_string(nbPizzas));
     while (nbPizzas > 0) {
-        debug.printOutput("Nb Pizzas #2: " + std::to_string(nbPizzas));
         if (streamCom_.empty() == false) {
             for (size_t i = 0; i < streamCom_.size(); ++i) {
                 while (infos == "OK" && nbPizzas > 0) {
-                    debug.printOutput("======> Kitchen already created ! " +
-                                      std::to_string(kitchensPid_[i]));
                     streamCom_[i].writeToKitchenBuffer(serializedOrder);
                     infos = streamCom_[i].readReceptionBuffer();
                     if (infos == "OK" && nbPizzas > 0) {
@@ -206,15 +187,12 @@ int Reception::sendPizzasToKitchens()
                 }
             }
         }
-        debug.printOutput("Nb Pizzas #3: " + std::to_string(nbPizzas));
         if (nbPizzas > 0) {
             if (createNewKitchenProcess(serializedOrder, nbPizzas)) {
                 return 1;
             }
         }
     }
-    debug.printOutput("=======> #2 There is " +
-                      std::to_string(streamCom_.size()) + " Kitchens");
     pizzas_.pop();
     return 0;
 }
